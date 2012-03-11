137a49108c77690a9be3341809bcf497bedb5b05
Felipe Astroza <felipe@astroza.cl>
2012-03-10 21:27:15 -0300

    inet routines are in iomplx_inet.[ch] now

---
+++ iomplx_inet.c
@@ -0,0 +1,46 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <iomplx.h>
+#include <unistd.h>
+
+int iomplx_inet_listen(iomplx_instance *mplx, const char *addr, unsigned short port, ev_call1 ev_accept, void *data)
+{
+	iomplx_item local_item, *item;
+	struct sockaddr_in *sa;
+	int ret;
+	int status = 1;
+
+	local_item.fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (local_item.fd == -1 )
+                return -1;
+
+	sa = (struct sockaddr_in *)&local_item.sa;
+	sa->sin_family = AF_INET;
+	sa->sin_port = htons(port);
+	sa->sin_addr.s_addr = inet_addr(addr);
+	local_item.sa_size = sizeof(struct sockaddr_in);
+
+	if(setsockopt(local_item.fd, SOL_SOCKET, SO_REUSEADDR, &status, sizeof(status)) == -1)
+		goto error;
+
+	ret = bind(local_item.fd, &local_item.sa, local_item.sa_size);
+	if(ret == -1)
+		goto error;
+
+	if(listen(local_item.fd, IOMPLX_CONF_BACKLOG) == -1)
+		goto error;
+
+	local_item.data = data;
+	local_item.new_filter = IOMPLX_READ;
+	iomplx_callbacks_init(&local_item);
+	local_item.cb.ev_accept = ev_accept;
+
+	item = iomplx_item_add(mplx, &local_item, 1);
+	if(!item)
+		goto error;
+
+	return 0;
+error:
+	close(local_item.fd);
+	return -1;
+}
---
+++ iomplx_inet.h
@@ -0,0 +1,6 @@
+#ifndef __IOMPLX_INET__
+#define __IOMPLX_INET__
+
+int iomplx_inet_listen(iomplx_instance *, const char *, unsigned short, ev_call1, void *);
+
+#endif
